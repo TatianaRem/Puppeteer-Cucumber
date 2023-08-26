d2d78561183793458a98bd809f98f1b9883fb75f
Tatiana Rem <tatiana13d@yandex.ru>
2023-08-26 13:53:28 +0600

    Puppeteer and Cucumber are ready to start

--- booking.test.js
+++ booking.test.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 let browser;
 let page;
@@ -6,7 +6,7 @@ let page;
 beforeEach(async () => {
 	browser = await puppeteer.launch();
 	page = await browser.newPage();
-	await page.goto("http://qamid.tmweb.ru/client/index.php");
+	await page.setDefaultNavigationTimeout(0);
 });
 
 afterEach(async () => {
@@ -14,8 +14,7 @@ afterEach(async () => {
 });
 
 const fillBookingForm = async (page, movieTimeSelector, movieDateSelector) => {
-
-	//Выбор даты 
+	//Выбор даты
 	await page.click(movieDateSelector);
 
 	//Выбор времени начала фильма
@@ -25,15 +24,16 @@ const fillBookingForm = async (page, movieTimeSelector, movieDateSelector) => {
 	await page.waitForNavigation();
 };
 
-module.exports = { fillBookingForm };
-
 describe("Booking cinema tickets", () => {
-	const movieDateSelector = `[data-time-stamp="1692997200"]`;
-	const movieTimeSelector = `[data-seance-id="178"]`;
+	beforeEach(async () => {
+		await page.goto("http://qamid.tmweb.ru/client/index.php");
+	}, 15000);
+	const movieDateSelector = `[data-time-stamp="1693256400"]`;
+	const movieTimeSelector = `[data-seance-id="176"]`;
 
 	test("Happy path booking the first", async () => {
 		await fillBookingForm(page, movieTimeSelector, movieDateSelector);
-		const seatSelector = 'div:nth-child(6) span:nth-child(5)'; //Выбор места
+		const seatSelector = "div:nth-child(6) span:nth-child(5)"; //Выбор места
 		await page.click(seatSelector);
 		const buttonSelector = "button";
 		await page.waitForSelector(buttonSelector, {
@@ -45,13 +45,16 @@ describe("Booking cinema tickets", () => {
 
 	test("Sad path booking", async () => {
 		await fillBookingForm(page, movieTimeSelector, movieDateSelector);
-		const seatSelector = 'div:nth-child(1) span:nth-child(9)'; //Выбор места
+		const seatSelector = "div :nth-child(2) span:nth-child(6)"; //Выбор места
 		await page.click(seatSelector);
 		const greyButtonSelector = "button";
 		await page.waitForSelector(greyButtonSelector, {
 			visible: false,
 		});
-		const actual = await page.$eval(greyButtonSelector, (link) => link.textContent);
+		const actual = await page.$eval(
+			greyButtonSelector,
+			(link) => link.textContent
+		);
 		expect(actual).toContain("Забронировать");
 	}, 15000);
 
@@ -61,16 +64,17 @@ describe("Booking cinema tickets", () => {
 
 		test("Happy path booking the second", async () => {
 			await fillBookingForm(page, movieTimeSelector, movieDateSelector);
-			const seatSelector = 'div:nth-child(3) span:nth-child(1)'; //Выбор места
+			const seatSelector = "div:nth-child(3) span:nth-child(1)"; //Выбор места
 			await page.click(seatSelector);
 			const buttonSelector = "button";
 			await page.waitForSelector(buttonSelector, {
 				visible: true,
 			});
-			const actual = await page.$eval(buttonSelector, (link) => link.textContent);
+			const actual = await page.$eval(
+				buttonSelector,
+				(link) => link.textContent
+			);
 			expect(actual).toContain("Забронировать");
 		}, 15000);
-
-
 	});
 });
--- features/step_definitions/booking_steps.js
+++ features/step_definitions/booking_steps.js
@@ -1,115 +1,118 @@
-const { Given, When, Then, After, Before } = require("cucumber");
+const {
+	Given,
+	When,
+	Then,
+	After,
+	Before
+} = require("cucumber");
 const puppeteer = require("puppeteer");
 const expect = require("chai").expect;
 
 const fillBookingForm = async (page, movieTimeSelector, movieDateSelector) => {
-  //Выбор даты
-  await page.click(movieDateSelector);
-  await page.waitForTimeout(1000);
+	//Выбор даты
+	await page.click(movieDateSelector);
+	await page.waitForTimeout(1000);
 
-  //Выбор времени начала фильма
-  await page.click(movieTimeSelector);
+	//Выбор времени начала фильма
+	await page.click(movieTimeSelector);
 
-  //Ожидание загрузки страницы
-  await page.waitForNavigation();
+	//Ожидание загрузки страницы
+	await page.waitForNavigation();
 };
 
 let browser;
 let page;
-const buttonSelector = "button";
-const greyButtonSelector = "button";
 let movieTimeSelector;
 let movieDateSelector;
 
 Before(async () => {
-  browser = await puppeteer.launch();
-  page = await browser.newPage();
-  await page.goto("http://qamid.tmweb.ru/client/index.php");
+	browser = await puppeteer.launch();
+	page = await browser.newPage();
+	await page.goto("http://qamid.tmweb.ru/client/index.php");
 
-  movieDateSelector = `[data-time-stamp="1692997200"]`;
-  movieTimeSelector = `[data-seance-id="178"]`;
+	movieDateSelector = `[data-time-stamp="1693256400"]`;
+	movieTimeSelector = `[data-seance-id="176"]`;
 });
 
 After(async () => {
-  await browser.close();
+	await browser.close();
 });
 
 Given("the user is on the booking page", async () => {});
 
 When("the user selects the movie date and time", async () => {
-  // movieDateSelector = `[data-time-stamp="1692997200"]`;
-  // movieTimeSelector = `[data-seance-id="178"]`;
-  await fillBookingForm(page, movieTimeSelector, movieDateSelector);
+	await fillBookingForm(page, movieTimeSelector, movieDateSelector);
 });
 
 When("the user chooses a seat", async () => {
-  const seatSelector = "div:nth-child(6) span:nth-child(5)";
-  await page.click(seatSelector);
+	const seatSelector = "div:nth-child(6) span:nth-child(5)";
+	await page.click(seatSelector);
 });
 
-Then(
-  "the user should see the active {string} button",
-  async function (Забронировать) {
-    const buttonSelector = "button";
-    await page.waitForSelector(buttonSelector, { visible: true });
-    await page.click(buttonSelector);
-    const actual = await page.$eval(buttonSelector, (link) => link.textContent);
-    expect(actual).to.contain(Забронировать);
-  }
-);
+Then("the user should see the active {string} button", async function(string) {
+	const buttonSelector = "button";
+	await page.waitForSelector(buttonSelector, {
+		visible: true
+	});
+	await page.click(buttonSelector);
+	const actual = await page.$eval(
+		buttonSelector,
+		(button) => button.textContent
+	);
+	expect(actual).to.contain(string);
+});
 
 When("the user chooses occupied seat", async () => {
-  const seatSelector = "div:nth-child(1) span:nth-child(9)";
-  await page.click(seatSelector);
+	const seatSelector = "div :nth-child(2) span:nth-child(9)";
+	await page.click(seatSelector);
 });
 
 Then(
-  "The user should see the disabled {string} button",
-  async function (Забронировать) {
-    const greyButtonSelector = "button";
+	"the user should see the disabled {string} button",
+	async function(string) {
+		const greyButtonSelector = "button";
 
-    await page.waitForSelector(greyButtonSelector);
+		await page.waitForSelector(greyButtonSelector);
 
-    const isButtonDisabled = await page.$eval(greyButtonSelector, (button) => button.disabled
-    );
+		await page.waitForTimeout(1000);
+		const isButtonDisabled = await page.$eval(
+			greyButtonSelector,
+			(button) => button.disabled
+		);
 
-    expect(isButtonDisabled).toBe(true);
-    const actualButtonText = await page.$eval(greyButtonSelector, (button) => button.textContent
-    );
+		expect(isButtonDisabled).to.be.true;
+		const actualButtonText = await page.$eval(
+			greyButtonSelector,
+			(button) => button.textContent
+		);
 
-    expect(actualButtonText).toContain(Забронировать);
-
-    return "pending";
-  }
+		expect(actualButtonText).to.contain(string);
+	}
 );
 
-
 Before(async () => {
-  browser = await puppeteer.launch();
-  page = await browser.newPage();
-  await page.goto("http://qamid.tmweb.ru/client/index.php");
-
-  const movieDateSelector = `[data-time-stamp="1692997200"]`;
-  const movieTimeSelector = `[data-seance-id="173"]`;
-});
-
-After(async () => {
-  await browser.close();
+	const movieDateSelector = `[data-time-stamp="1692997200"]`;
+	const movieTimeSelector = `[data-seance-id="173"]`;
 });
 
 When("the user selects a different movie date and time", async () => {
-  await fillBookingForm(page, movieTimeSelector, movieDateSelector);
+	await fillBookingForm(page, movieTimeSelector, movieDateSelector);
 });
 
 When("the user selects a seat", async () => {
-  const seatSelector = "div:nth-child(3) span:nth-child(1)";
-  await page.click(seatSelector);
+	const seatSelector = "div:nth-child(3) span:nth-child(1)";
+	await page.click(seatSelector);
 });
 
-Then("the user should see the {string} button", async function (Забронировать) {
-  const buttonSelector = "button";
-  await page.waitForSelector(buttonSelector, { visible: true });
-  await page.click(buttonSelector);
-  const actual = await page.$eval(buttonSelector, (link) => link.textContent);
-  expect(actual).to.contain(Забронировать);
+Then("the user should see the {string} button", async function(string) {
+	const buttonSelector = "button";
+	await page.waitForSelector(buttonSelector, {
+		visible: true
+	});
+	await page.click(buttonSelector);
+	const actual = await page.$eval(
+		buttonSelector,
+		(button) => button.textContent
+	);
+	expect(actual).to.contain(string);
 });
